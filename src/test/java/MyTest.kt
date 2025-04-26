2bc6f4ea9c5b1197384e81538b97f5942b0a54a9
IGramyak <IGramyak@inno.tech>
2025-04-26 10:59:53 +0300

    Test add

--- src/test/java/MyTest.kt
+++ src/test/java/MyTest.kt
@@ -2,16 +2,19 @@ import org.gis.controller.HealthCheckController
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(HealthCheckController::class)
 class MyTest {
 
-    @Autowired
-    private lateinit var mockMvc: MockMvc
+//    @Autowired
+//    private lateinit var mockMvc: MockMvc
 
 
     @Test
     fun myTest() {
-
+//        mockMvc.perform(MockMvcRequestBuilders.post("/hi")).andExpect(status().isOk())
     }
 }
